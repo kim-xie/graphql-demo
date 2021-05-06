1ee36084c3d3314b2fb6bf9064991f6975ac00eb
kim <1134771121@qq.com>
2021-05-06 08:12:56 +0800

    first commit

---
+++ server/express-graphql-server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+// 使用 GraphQL Schema Language 创建一个 schema
+const schema = buildSchema(`
+  type Query {
+    hello: String
+    world: String
+  }
+`);
+
+// root 提供所有 API 入口端点相应的解析器函数
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+  world: () => {
+    return ' world!';
+  },
+};
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(4000,() => {
+  console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+});
